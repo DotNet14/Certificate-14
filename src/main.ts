b99d3299e1fe7f3f2345a3ae4f112d5048140bec
DotNet14 <naizuddin12@gmail.com>
2026-01-02 00:52:10 +0530

    Consumer Protection Acknowledgement

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-
-bootstrapApplication(App, appConfig)
+import { Request } from './app/request/request';
+bootstrapApplication(Request, appConfig)
   .catch((err) => console.error(err));
